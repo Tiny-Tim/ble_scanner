8b5ddc96c4554e339f34300356d4c05b955d14fd
chipk215 <chipk215@msn.com>
2013-02-12 09:11:09 -0700

    refactoring and documentation

--- BLE_Scanner/BLEHeartRateDemoViewController.m
+++ BLE_Scanner/BLEHeartRateDemoViewController.m
@@ -12,8 +12,10 @@
 
 @interface BLEHeartRateDemoViewController ()
 
+// NSLogging control
 @property (nonatomic) BOOL debug;
 
+// ImageView for heart beat animation
 @property (weak, nonatomic) IBOutlet UIImageView *heartBeatImage;
 
 // Label which displays peripheral status and activity.
@@ -22,15 +24,50 @@
 // Spinner activity indicator which is active when device is being accessed.
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *peripheralStatusSpinner;
 
+// Displays the numeric heart rate value in beats per minute
 @property (weak, nonatomic) IBOutlet UILabel *heartRateMeasureLabel;
 
+// Last measurement received from device
 @property (nonatomic)NSUInteger lastMeasurement;
 
+// state variable which indicates whether heart beat animation is active
 @property (nonatomic)BOOL animationStarted;
+
+// holds the heart beat animation image frames
+@property (nonatomic, strong) NSArray *heartBeatAnimationFrames;
 
 @end
 
 @implementation BLEHeartRateDemoViewController
+
+
+#pragma mark- Properties
+
+/*
+ *
+ * Method Name:  setLastMeasurement
+ *
+ * Description:  Setter for lastMeasurement which also updates UI when changed
+ *
+ * Parameter(s): lastMeasurement - new value for property
+ *
+ */
+-(void)setLastMeasurement:(NSUInteger)lastMeasure
+{
+    if (_lastMeasurement != lastMeasure)
+    {
+        _lastMeasurement = lastMeasure;
+        
+        //Update UI 
+        self.heartRateMeasureLabel.text = [NSString stringWithFormat:@"Heart Rate:  %i",lastMeasure];
+        
+    }
+}
+
+
+
+#pragma mark- View Controller Lifecycle
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -41,6 +78,71 @@
     return self;
 }
 
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    // initialize debug,animation, and last read measurement state variables
+    self.debug = YES;
+    
+    self.animationStarted = NO;
+    
+    self.lastMeasurement = 0;
+    
+    // clear the UI measurement label
+    self.heartRateMeasureLabel.text = @"";
+    
+    // set the peripheral delegate to self
+    self.heartRateService.peripheral.delegate =self;
+    
+    // set up the animation image data
+    [self setupHeartBeatAnimation];
+    
+    // display the peripheral connection status
+    [self setConnectionStatus];
+    
+    // subscribe for notifications to changes of heart rate
+    [self enableForHeartRateMeasurementNotifications: YES];
+    
+    
+}
+
+
+/*
+ *
+ * Method Name:  viewWillDisappear
+ *
+ * Description:  Ensure view controller is not being updated when not in view.
+ *
+ * Parameter(s): animated - system parameter controlling view transition
+ *
+ */
+-(void) viewWillDisappear:(BOOL)animated
+{
+    self.heartRateService.peripheral.delegate = nil;
+    
+    [self stopHeartBeatAnimation];
+    
+    [self enableForHeartRateMeasurementNotifications: NO];
+    
+    [super viewWillDisappear:animated];
+    
+}
+
+
+// template code
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+#pragma mark- Private Methods
 
 /*
  *
@@ -91,8 +193,6 @@
 
 
 
-
-
 -(void)enableForHeartRateMeasurementNotifications : (BOOL) enable
 {
     // Check to see if peripheral has retrieved characteristics
@@ -131,21 +231,28 @@
 }
 
 
-- (void)viewDidLoad
+
+-(void)startHeartBeatAnimation
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    self.heartBeatImage.animationImages = self.heartBeatAnimationFrames;
+    self.heartBeatImage.animationDuration = 0.6;
+    [self.heartBeatImage startAnimating];
+    self.animationStarted = YES;
+}
+
+
+-(void)stopHeartBeatAnimation
+{
+    [self.heartBeatImage stopAnimating];
+    self.heartBeatImage.animationImages = nil;
+    self.animationStarted = NO;
     
-    _debug = YES;
-    
-    _animationStarted = NO;
-    
-    self.lastMeasurement = 0;
-    
-    self.heartRateMeasureLabel.text = @"";
-    
-    self.heartRateService.peripheral.delegate =self;
-    
+    // display a non-moving heart image until device begins sending measurements
+    self.heartBeatImage.image = self.heartBeatAnimationFrames[0];
+}
+
+-(void)setupHeartBeatAnimation
+{
     int numberOfFrames = 10;
     NSMutableArray *imagesArray = [NSMutableArray arrayWithCapacity:numberOfFrames];
     
@@ -157,36 +264,35 @@
         
     }
     
-    self.heartBeatImage.animationImages = imagesArray;
-    self.heartBeatImage.animationDuration = 0.6;
-   
+    self.heartBeatAnimationFrames = [imagesArray copy];
     
-    [self setConnectionStatus];
-    
-    [self enableForHeartRateMeasurementNotifications: YES];
-    
+    // display a non-moving heart image until device begins sending measurements
+    self.heartBeatImage.image = self.heartBeatAnimationFrames[0];
     
 }
 
 
 
--(void) viewWillDisappear:(BOOL)animated
+/*
+ *
+ * Method Name:  processHeartRateMeasurement
+ *
+ * Description:  Processes the heart rate measurement received from the device. Animations are started if needed and the heart rate measurement may be further processed if needed by the application.
+ *
+ * Parameter(s): beatsPerMinute - the heart rate measurement in beats per minute
+ *
+ */
+-(void)processHeartRateMeasurement: (NSUInteger) beatsPerMinute
 {
-    self.heartRateService.peripheral.delegate = nil;
+    // start heart beat animation if idle
+    if (! self.animationStarted)
+    {
+        [self startHeartBeatAnimation];
+    }
     
-    [self.heartBeatImage stopAnimating];
+    // Updating this property will also update the UI if needed
+    self.lastMeasurement = beatsPerMinute;
     
-    [self enableForHeartRateMeasurementNotifications: NO];
-    
-    [super viewWillDisappear:animated];
-    
-}
-
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
@@ -197,47 +303,53 @@
     if (self.debug) NSLog(@"didUpdateNotificationStateForCharacteristic invoked");
 }
 
+
+
+/*
+ *
+ * Method Name:  didUpdateValueForCharacteristic
+ *
+ * Description:  The usual characteristic reading functionality associated with this CBPeripheralDelegate method. Identifies which characteristic is being updated and then handles processing the data.
+ *
+ * Parameter(s): peripheral - the peripheral sending the data
+ *               characteristic - the chracteristic being updated
+ *               error - any error generated during the updating of the characteristic
+ *
+ */
 - (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
 {
-    
-    
     if (!error)
     {
         if (self.debug) NSLog(@"Characteristic value  updated.");
         
-        const uint8_t *reportData = [characteristic.value bytes];
-        uint16_t bpm = 0;
-        
-        if ((reportData[0] & 0x01) == 0)
+        // Determine which characteristic was updated
+        /* Updated value for heart rate measurement received */
+        if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:HEART_RATE_MEASUREMENT_CHARACTERISTIC]])
         {
-            /* uint8 bpm */
-            bpm = reportData[1];
-        }
-        else
-        {
-            /* uint16 bpm */
-            bpm = CFSwapInt16LittleToHost(*(uint16_t *)(&reportData[1]));
-        }
-        
-        NSLog(@"Heart Rate Measurement Rcvd: %i",bpm);
-        if (! self.animationStarted)
-        {
-            self.animationStarted = YES;
-            if (bpm < 20) bpm = 20;
-            [self.heartBeatImage startAnimating];
-        }
-        if (bpm != self.lastMeasurement)
-        {
-            self.lastMeasurement = bpm;
-             if (bpm < 20) bpm = 20;
+            const uint8_t *reportData = [characteristic.value bytes];
+            NSUInteger bpm = 0;
             
-            self.heartRateMeasureLabel.text = [NSString stringWithFormat:@"Heart Rate:  %i",bpm];
-           
+            // least sig bit of first byte encodes whether measurement is 1 or 2 bytes
+            if ((reportData[0] & 0x01) == 0)
+            {
+                /* uint8 bpm */
+                bpm = reportData[1];
+            }
+            else
+            {
+                /* uint16 bpm */
+                bpm = CFSwapInt16LittleToHost(*(uint16_t *)(&reportData[1]));
+            }
+            if (self.debug) NSLog(@"Heart Rate Measurement Rcvd: %i",bpm);
+            [self processHeartRateMeasurement:bpm];
         }
     }
+    else
+    {
+        NSLog(@"Error reading characteristic: %@", error.description);
+    };
     
     [self setConnectionStatus];
-    
 }
 
 
